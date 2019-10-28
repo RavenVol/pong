72a17288fae47cd3c66503799c6496a39b77744a
RavenVol <r.volianiuk@gmail.com>
2019-10-28 16:11:55 +0200

    Updates

---
+++ precache-manifest.7d26721121c0eec6fd9cf88976072deb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe0d92c35a1db5eb64905b68c96378e",
+    "url": "/pong/index.html"
+  },
+  {
+    "revision": "4c30525a3702508865d1",
+    "url": "/pong/static/css/main.2f5e8bb3.chunk.css"
+  },
+  {
+    "revision": "4560886a3cb028e7a3be",
+    "url": "/pong/static/js/2.8a586615.chunk.js"
+  },
+  {
+    "revision": "4c30525a3702508865d1",
+    "url": "/pong/static/js/main.2927c062.chunk.js"
+  },
+  {
+    "revision": "a417f954ab3244e00dfb",
+    "url": "/pong/static/js/runtime~main.a500ca0d.js"
+  }
+]);
